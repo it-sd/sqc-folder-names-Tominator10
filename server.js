ee2b4755053512701451a8cc6d5c21a52ce59f48
Tominator10 <Tominator10@users.noreply.github.com>
2023-02-24 17:55:26 -0600

    Handled testing with jasmine and nyc

--- server.js
+++ server.js
@@ -12,14 +12,4 @@ const newFolderName = function (folders) {
 }
 
 // TESTS
-const arrTest1 = ['Random Folder', 'Dog Pictures', 'Snowboards']
-console.log('New Folder <- Expected')
-console.log(newFolderName(arrTest1) + ' <- Result\n')
-
-const arrTest2 = ['Random Folder', 'New Folder', 'Snowboards']
-console.log('New Folder (2) <- Expected')
-console.log(newFolderName(arrTest2) + ' <- Result\n')
-
-const arrTest3 = ['New Folder (2)', 'New Folder', 'New Folder (4)']
-console.log('New Folder (3) <- Expected')
-console.log(newFolderName(arrTest3) + ' <- Result')
+module.exports = { newFolderName }
